5f7cb961e9d9aa95a7058767aa39ee8ff9e95b2f
muhammadnabil9263 <muhammadnabeel9263@gmail.com>
2024-10-04 17:49:32 +0300

    finish stock controller

--- Controllers/StocksController.cs
+++ Controllers/StocksController.cs
@@ -1,5 +1,8 @@
 ﻿using api.Data;
+using api.DTOs;
+using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
+using System;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -16,38 +19,88 @@ namespace api.Controllers
             _context = context;
         }
 
-        // GET: api/<StocksController>
-        [HttpGet("getAll")]
-        public IActionResult GetAll ()
+        // GET: api/stocks
+        [HttpGet]
+        public IActionResult GetAll()
         {
-            var stocks = _context.Stocks.ToList(); 
-            return Ok(stocks);
+            var stocks = _context.Stocks.ToList();
+            var stockDTOs = stocks.Select(stock => StockMapper.ToDTO(stock)).ToList();
+            return Ok(stockDTOs);
         }
 
-
-        // GET api/<StocksController>/5
+        // GET: api/stocks/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public IActionResult GetById(int id)
         {
-            return "value";
+            var stock = _context.Stocks.FirstOrDefault(s => s.Id == id);
+            if (stock == null)
+            {
+                return NotFound(new { message = $"Stock with id {id} not found." });
+            }
+
+            var stockDTO = StockMapper.ToDTO(stock);
+            return Ok(stockDTO);
         }
 
-        // POST api/<StocksController>
+        // POST: api/stocks
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult Post([FromBody] StockDTO stockDTO)
         {
+            if (stockDTO == null)
+            {
+                return BadRequest(new { message = "Invalid stock data." });
+            }
+
+            var stock = StockMapper.ToModel(stockDTO);
+            _context.Stocks.Add(stock);
+            _context.SaveChanges();
+
+            return Ok(new { message = "Stock created successfully.",stock = stockDTO });
         }
 
-        // PUT api/<StocksController>/5
+        // PUT: api/stocks/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public IActionResult Put(int id, [FromBody] StockDTO stockDTO)
         {
+            if (stockDTO == null || stockDTO.Id != id)
+            {
+                return BadRequest();
+            }
+
+            var stock = _context.Stocks.FirstOrDefault(s => s.Id == id);
+            if (stock == null)
+            {
+                return NotFound(new { message = $"Stock with id {id} not found." });
+            }
+
+            // Update stock properties
+            stock.Symbol = stockDTO.Symbol;
+            stock.CompanyName = stockDTO.CompanyName;
+            stock.Purchase = stockDTO.Purchase;
+            stock.LastDiv = stockDTO.LastDiv;
+            stock.Industry = stockDTO.Industry;
+            stock.MarketCap = stockDTO.MarketCap;
+
+            _context.Stocks.Update(stock);
+            _context.SaveChanges();
+
+            return Ok(new { message = "Stock updated successfully.", stock = stockDTO });
         }
 
-        // DELETE api/<StocksController>/5
+        // DELETE: api/stocks/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
+            var stock = _context.Stocks.FirstOrDefault(s => s.Id == id);
+            if (stock == null)
+            {
+                return NotFound(new { message = $"Stock with id {id} not found." });
+            }
+
+            _context.Stocks.Remove(stock);
+            _context.SaveChanges();
+
+            return Ok(new { message = "Stock deleted successfully." });
         }
     }
 }
---
+++ Mappers/StockMapper.cs
@@ -0,0 +1,35 @@
+﻿using api.DTOs;
+using api.Models;
+
+namespace api.Mappers;
+
+public class StockMapper
+{
+    public static StockDTO ToDTO(Stock stock)
+    {
+        return new StockDTO
+        {
+            Id = stock.Id,
+            Symbol = stock.Symbol,
+            CompanyName = stock.CompanyName,
+            Purchase = stock.Purchase,
+            LastDiv = stock.LastDiv,
+            Industry = stock.Industry,
+            MarketCap = stock.MarketCap
+        };
+    }
+
+    public static Stock ToModel(StockDTO stockDTO)
+    {
+        return new Stock
+        {
+            Id = stockDTO.Id,
+            Symbol = stockDTO.Symbol,
+            CompanyName = stockDTO.CompanyName,
+            Purchase = stockDTO.Purchase,
+            LastDiv = stockDTO.LastDiv,
+            Industry = stockDTO.Industry,
+            MarketCap = stockDTO.MarketCap
+        };
+    }
+}
